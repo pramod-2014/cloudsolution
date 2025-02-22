22f4ed712efc94f02fa00422a7370eea6c1402e8
pramod kumar <prmd777@rediffmail.com>
2025-02-22 20:32:51 +0530

    push cloudsolution project 22 Feb

---
+++ src/main/java/com/cloudsolution/cloudsolution/CloudsolutionApplication.java
@@ -0,0 +1,13 @@
+package com.cloudsolution.cloudsolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudsolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudsolutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudsolution/cloudsolution/CloudsolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudsolution.cloudsolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudsolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
